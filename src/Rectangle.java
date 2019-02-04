2a4100ec628e9bccc9b4b15a7fa57c16576c19fc
hdiab22 <42975537+HadyTheDiabMan@users.noreply.github.com>
2019-02-04 12:43:30 -0500

    Add files via upload

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,24 @@
-
-public class Rectangle {
-
+/**
+ * 
+ * @author hdiab22
+ *
+ */
+public class Rectangle 
+{
+//fields for dimensions only
+	//methods:
+	//setwidth
+	//setheight
+	//get width
+	//getheight
+	//getdiagonal
+	private double length=0;
+	private double width=0;
+	public double setwidth(double num1)
+	{
+		width=num1;
+		return width;
+	}
+	
+	
 }
